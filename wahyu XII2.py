509b1014485ffa78c8ae600f349fd43865cd65e1
Wahyu Romadlon <147460186+whyurrr@users.noreply.github.com>
2024-08-19 08:43:18 +0700

    Create wahyu XII2.py

---
+++ wahyu XII2.py
@@ -0,0 +1,11 @@
+# Deklarasi variabel
+angka1 = 25
+angka2 = 7
+
+# Operasi modulus
+sisa = angka1 % angka2
+
+# Menampilkan hasil dengan print
+print("Angka pertama:", angka1)
+print("Angka kedua:", angka2)
+print("Sisa dari pembagian", angka1, "dengan", angka2, "adalah", sisa)
